316c63be1e6ecf52e556d341b0e1c6ecfe1857b7
Schamotnic <81876870+Schamotnic@users.noreply.github.com>
2022-01-21 12:01:39 +0000

    Deploying to gh-pages from @ Schamotnic/goit-js-hw-08@46aec7763a5edd3dcf98ce368915e32b94052d1e 🚀

--- 01-gallery.8ffc28be.js
+++ 01-gallery.8ffc28be.js
@@ -8,4 +8,4 @@ var t=arguments[3];function e(t){return(e="function"==typeof Symbol&&"symbol"==t
 },{}],"TfQ5":[function(require,module,exports) {
 "use strict";var e=i(require("simplelightbox"));require("simplelightbox/dist/simple-lightbox.min.css");var l=require("./gallery-items");function i(e){return e&&e.__esModule?e:{default:e}}const t=l.galleryItems.map(e=>`\n   <a class="gallery__item" href="${e.original}">\n   <img\n   class="gallery__image"\n   src="${e.preview}" \n   alt="${e.description}"\n   />\n    </a>\n    `).join("");document.querySelector(".gallery").innerHTML=t;const a=new e.default(".gallery a",{captionsData:"alt",captionDelay:250,captionPosition:"bottom"});console.log(l.galleryItems);
 },{"simplelightbox":"yx6k","simplelightbox/dist/simple-lightbox.min.css":"kz9M","./gallery-items":"q5Rn"}]},{},["TfQ5"], null)
-//# sourceMappingURL=/parcel-project-template/01-gallery.8ffc28be.js.map
+//# sourceMappingURL=/goit-js-hw-08/01-gallery.8ffc28be.js.map
