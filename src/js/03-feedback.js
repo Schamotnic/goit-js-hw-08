6d5a8a352b859b8d5a5dc64e1492d67b6020c162
Alecsandr <bolatschewez@gmail.com>
2022-02-04 20:11:33 +0200

    Виправив зауваження по 3 задачі

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,7 +5,7 @@ const formeLement = document.querySelector(".feedback-form");
 formeLement.addEventListener('input',throttle(onFormSubmit, 500) );
 formeLement.addEventListener('submit',formInput);
 
-const forObject = {};
+let forObject = {};
 savedForm();
 
 function onFormSubmit (evt) {
@@ -15,25 +15,46 @@ function onFormSubmit (evt) {
    forObject[evt.target.name] = evt.target.value;
    console.log(forObject);
    localStorage.setItem("feedback-form-state",JSON.stringify(forObject));
+   
 };
 
 function formInput (evt){
    evt.preventDefault();
-   evt.target.reset()
-};  
-
-function savedForm(){
-   let savedFormStorege = localStorage.getItem("feedback-form-state")
-   if(savedFormStorege){
-      savedFormStorege = JSON.parse(savedFormStorege)
-      Object.entries(savedFormStorege).forEach(([name, value]) =>{
-         formeLement.elements[name].value = value; 
-          
-      }) ;
-      
+   // evt.target.reset()
+   if (formeLement.email.value === '' || formeLement.message.value === '') {
+      alert('All fields are required!');
+   } else {
+      localStorage.removeItem("feedback-form-state")
+      evt.target.reset();
    }
+ };
+      
+ function savedForm(evt){
+    let savedFormStorege = localStorage.getItem("feedback-form-state")
+    if(savedFormStorege){
+       savedFormStorege = JSON.parse(savedFormStorege)
+       Object.entries(savedFormStorege).forEach(([name, value]) =>{
+          formeLement.elements[name].value = value; 
+       });
+    }
+ };
+     
+      
+
+
+      
+      
+      
    
-   };
+             
+         
+      
+
+   
+
+
+  
+
   
 
 
